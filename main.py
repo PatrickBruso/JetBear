e322d645f47043270c4bdb6c2897826a0e8a4326
Patrick <pkbruso@gmail.com>
2021-06-29 15:01:49 -0700

    Implemented splash screen function

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 
 pygame.display.set_caption("JetBear! The Game!")
 BG_IMAGE = pygame.image.load(os.path.join('assets/images', 'background_wide.png'))
+SPLASH = pygame.image.load(os.path.join('assets/images', 'Splash.png'))
+START = pygame.image.load(os.path.join('assets/images', 'start.png'))
 WIDTH = BG_IMAGE.get_width()
 HEIGHT = BG_IMAGE.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,10 +18,27 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 BEAR_IMAGE = pygame.image.load(os.path.join('assets/images', 'bear.png'))
 
 
+def splash():
+    WIN.blit(SPLASH, (0, 0))
+    button = WIN.blit(START, (WIDTH / 2.60, HEIGHT / 1.75))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                if button.collidepoint(pos):
+                    draw_window()
+        pygame.display.update()
+    pygame.quit()
+
+
 def draw_window():  # function to draw on window
+    WIN.fill((0, 0, 0))
     WIN.blit(BG_IMAGE, (0, 0))  # load background image
     WIN.blit(BEAR_IMAGE, (WIDTH // 4, HEIGHT // 2))  # should be sprite
-    pygame.display.update()  # update the window
 
 
 def main():
@@ -30,10 +49,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        pygame.display.update()
+        splash()
 
     pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    splash()
