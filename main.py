659be7080413567c550af372b2280a58458e654b
Patrick Bruso <pkbruso@gmail.com>
2022-06-13 21:33:58 -0700

    Updated naming of constants to all caps

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ Copyright 2021 Patrick Bruso
 import pygame as pg
 import os
 
-pg.display.set_caption("JetBear! The Game!")
-
 # load assets
-bg_image = pg.image.load(os.path.join('assets/images', 'background_wide.png'))
-splash_image = pg.image.load(os.path.join('assets/images', 'Splash.png'))
-start = pg.image.load(os.path.join('assets/images', 'start.png'))
-bear_image = pg.image.load(os.path.join('assets/images', 'bear.png'))
+BG_IMAGE = pg.image.load(os.path.join('assets/images', 'background_wide.png'))
+SPLASH_IMAGE = pg.image.load(os.path.join('assets/images', 'Splash.png'))
+START = pg.image.load(os.path.join('assets/images', 'START.png'))
+BEAR_IMAGE = pg.image.load(os.path.join('assets/images', 'bear.png'))
 
 # constants
-width = bg_image.get_width()
-height = bg_image.get_height()
+WIDTH = BG_IMAGE.get_width()
+HEIGHT = BG_IMAGE.get_height()
 FPS = 60
 
+# Set game window caption
+pg.display.set_caption("JetBear! The Game!")
+
 # set display call
-win = pg.display.set_mode((width, height))
+win = pg.display.set_mode((WIDTH, HEIGHT))
 
 
 class Bear(object):
     def __init__(self):
-        self.image = bear_image
-        self.x = width / 4
-        self.y = height / 2
+        self.image = BEAR_IMAGE
+        self.x = WIDTH / 4
+        self.y = HEIGHT / 2
 
     def key_handle(self):
         key = pg.key.get_pressed()
@@ -51,8 +52,8 @@ def main():
     """
     Main game loop that implements splash screen and calls game function upon button click
     """
-    win.blit(splash_image, (0, 0))
-    button = win.blit(start, (width / 2.60, height / 1.75))
+    win.blit(SPLASH_IMAGE, (0, 0))
+    button = win.blit(START, (WIDTH / 2.60, HEIGHT / 1.75))
     clock = pg.time.Clock()
     bear = Bear()
 
@@ -65,7 +66,7 @@ def main():
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 pos = pg.mouse.get_pos()
                 if button.collidepoint(pos):
-                    win.blit(bg_image, (0, 0))
+                    win.blit(BG_IMAGE, (0, 0))
 
         bear.draw(win)
         bear.key_handle()
